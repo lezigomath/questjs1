0891acb51014b137e26ac6463aca180749cdec47
Yann Mongoin <46783589+lezigomath@users.noreply.github.com>
2019-03-11 11:06:47 +0100

    Add files via upload

--- hello-wilder.js
+++ hello-wilder.js
@@ -1,10 +1,10 @@
 /* hello-wilder.js */
 
 // Define some variables
-const titreFilm = 'Enter the void';
+const titleMoovie = 'Enter the void';
 const real = 'Gaspard Noe';
-const annee = '2009';
-const message = titreFilm + ', sorti en '+ annee +', a été réalisé par ' + real + " et c'est un film de fou !";
+const year = '2009';
+const message = titleMoovie + ', sorti en '+ year +', a été réalisé par ' + real + " et c'est un film de fou !";
 
 // Display a popup
 alert(message);
